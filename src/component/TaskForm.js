e2c4f274b56eafff7678bb0e5247b82faa9e1b96
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-04-12 21:59:04 -0400

    time is fusturating

--- src/component/TaskForm.js
+++ src/component/TaskForm.js
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 
-
 function TaskForm({ onAddTask }) {
-
-    //\\* useState, but as an object* //\\
   const [task, setTask] = useState({
     date: '',
     startTime: '',
@@ -11,26 +8,42 @@ function TaskForm({ onAddTask }) {
     reminder: ''
   });
 
-      //\\* Form Change *//\\
   const handleFormChange = (event) => {
     const { name, value } = event.target;
-    console.log()
-    setTask((previousTask) => ({ ...previousTask, [name]: value }));
+  
+    // split date into year, month, day
+    const [year, month, day] = value.split('-');
+    let formattedDate = task.date;
+  
+    // if both year and day are selected, format the date
+    if (year && day) {
+      const dateObj = new Date(year, month - 1, day);
+      formattedDate = dateObj.toISOString().split('T')[0];
+    }
+  
+    setTask((previousTask) => ({
+      ...previousTask,
+      [name]: value,
+      date: formattedDate
+    }));
   };
+  
+  
 
-
-    //\\* Submit POST Functionality, but inheriting onAddTask property *//\\
-  const handleFormSubmit = async (event) => {    
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
-    //todo: set the default date today local
-    const startTime = new Date(`2000-01-01T${task.startTime}`);
-    const endTime = new Date(`2000-01-01T${task.endTime}`);
-    task.startTime = startTime.toLocaleDateString([],{ hour12:true, hour: 'numeric', minute: 'numeric'});
-    task.endTime = startTime.toLocaleDateString([],{ hour12:true, hour: 'numeric', minute: 'numeric'});
-
-
-      await onAddTask(task);
+      // Format the date
+      const date = new Date(task.date).toISOString().split('T')[0];
+  
+      // Format the start and end times
+      const startTime = new Date(`2000-01-01T${task.startTime}`);
+      const endTime = new Date(`2000-01-01T${task.endTime}`);
+      task.startTime = startTime.toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' });
+      task.endTime = endTime.toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' });
+  
+      // Send the formatted task object to the onAddTask function
+      await onAddTask({ ...task, date });
       setTask({
         date: '',
         startTime: '',
@@ -42,64 +55,63 @@ function TaskForm({ onAddTask }) {
       alert(`Error: ${error.message}`);
     }
   };
+  
 
   const today = new Date().toISOString().split('T')[0];
 
-
-    //\\* HTML SECTION *//\\
   return (
     <div className="task-form">
-       <h2>Add a New Task</h2>
+      <h2>Add a New Task</h2>
       <form onSubmit={handleFormSubmit}>
-       <div className="form-group">
+        <div className="form-group">
           <label htmlFor="date">Date</label>
-           <input
-             type="date"
-             id="date"
-             name="date"
+          <input
+            type="date"
+            id="date"
+            name="date"
             value={task.date}
-            min={ today }
-            max="2030-12-31"
-           onChange={handleFormChange}
+            min={today}
+            max="2040-12-31"
+            onChange={handleFormChange}
             required
-           />
-                    </div>
-         <div className="form-group">
-           <label htmlFor="start-time">Start Time</label>
-           <input
-             type="time"
-             id="start-time"
-             name="startTime"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="start-time">Start Time</label>
+          <input
+            type="time"
+            id="start-time"
+            name="startTime"
             value={task.startTime}
-             onChange={handleFormChange}
-             required
-           />
-         </div>
-         <div className="form-group">
-           <label htmlFor="end-time">End Time</label>
-           <input
+            onChange={handleFormChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="end-time">End Time</label>
+          <input
             type="time"
             id="end-time"
             name="endTime"
             value={task.endTime}
-             onChange={handleFormChange}
-             required
-           />
-         </div>
-         <div className="form-group">
-           <label htmlFor="reminder">Reminder</label>
-           <textarea
-             id="reminder"
-             name="reminder"
-             value={task.reminder}
-             onChange={handleFormChange}
+            onChange={handleFormChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reminder">Reminder</label>
+          <textarea
+            id="reminder"
+            name="reminder"
+            value={task.reminder}
+            onChange={handleFormChange}
             required
           ></textarea>
         </div>
         <button type="submit">Add Task</button>
       </form>
     </div>
-  
   );
 }
+
 export default TaskForm;
