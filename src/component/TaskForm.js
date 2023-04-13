35b7f21d262e908201db105962232a03fda87cd3
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-04-13 19:51:30 -0400

    fixed time bugs...

--- src/component/TaskForm.js
+++ src/component/TaskForm.js
@@ -9,18 +9,38 @@ function TaskForm({ onAddTask }) {
     reminder: ''
   });
 
+  // const handleFormChange = (event) => {
+  //   const { name, value } = event.target;
+
+  //   // Parse the input date value using Moment.js and format it
+  //   const formattedDate = moment(value).format('MMM Do, YYYY');
+
+  //   setTask((previousTask) => ({
+  //     ...previousTask,
+  //     [name]: value,
+  //     date: formattedDate
+  //   }));
+  // };
+
   const handleFormChange = (event) => {
     const { name, value } = event.target;
-
-    // Parse the input date value using Moment.js and format it
-    const formattedDate = moment(value).format('MMM Do, YYYY');
-
-    setTask((previousTask) => ({
-      ...previousTask,
-      [name]: value,
-      date: formattedDate
-    }));
+  
+    if (name === "date") {
+      // Parse the input date value using Moment.js and format it
+      const formattedDate = moment(value).format("MMM Do, YYYY");
+      setTask((previousTask) => ({
+        ...previousTask,
+        [name]: value,
+        date: formattedDate,
+      }));
+    } else {
+      setTask((previousTask) => ({
+        ...previousTask,
+        [name]: value,
+      }));
+    }
   };
+  
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
